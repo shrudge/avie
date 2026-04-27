c3d25f5e2087de10a527e44d1fa7bba64b77cbb3
Meet Balani <74920401+shrudge@users.noreply.github.com>
2026-04-27 17:21:25 +0530

    plugin parsing

--- Plugins/AviePlugin/AviePlugin.swift
+++ Plugins/AviePlugin/AviePlugin.swift
@@ -7,6 +7,14 @@ struct AviePlugin: CommandPlugin {
         let avieTool = try context.tool(named: "avie")
         var processArgs = ["audit", "--path", context.package.directory.string]
 
+        var argExtractor = ArgumentExtractor(arguments)
+        if let format = argExtractor.extractOption(named: "format").first {
+            processArgs += ["--format", format]
+        }
+        if argExtractor.extractFlag(named: "ci") > 0 {
+            processArgs.append("--ci")
+        }
+
         let process = Process()
         process.executableURL = URL(fileURLWithPath: avieTool.path.string)
         process.arguments = processArgs
