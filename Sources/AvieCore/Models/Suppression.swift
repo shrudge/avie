55749ba73bbe580615a2b76f57e59620376fb054
Meet Balani <74920401+shrudge@users.noreply.github.com>
2026-04-27 15:52:08 +0530

    suppression loading done

--- Sources/AvieCore/Models/Suppression.swift
+++ Sources/AvieCore/Models/Suppression.swift
@@ -1,8 +1,17 @@
+import Foundation
+
 public struct Suppression: Codable, Sendable {
     public let key: String
     public let reason: String
     public let addedBy: String
     public let addedAt: String
+
+    public init(key: String, reason: String, addedBy: String, addedAt: String) {
+        self.key = key
+        self.reason = reason
+        self.addedBy = addedBy
+        self.addedAt = addedAt
+    }
 }
 
 public struct SuppressionFile: Codable, Sendable {
@@ -11,4 +20,26 @@ public struct SuppressionFile: Codable, Sendable {
     public static let fileName = "avie-suppress.json"
 
     public init() {}
+
+    public static func load(from directory: URL) throws -> SuppressionFile {
+        let fileURL = directory.appendingPathComponent(fileName)
+        guard FileManager.default.fileExists(atPath: fileURL.path) else {
+            return SuppressionFile()
+        }
+        let data = try Data(contentsOf: fileURL)
+        return try JSONDecoder().decode(SuppressionFile.self, from: data)
+    }
+
+    public func save(to directory: URL) throws {
+        let fileURL = directory.appendingPathComponent(Self.fileName)
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
+        let data = try encoder.encode(self)
+        try data.write(to: fileURL, options: .atomic)
+    }
+}
+
+public func applySuppression(_ findings: [Finding], suppressions: SuppressionFile) -> [Finding] {
+    let keys = Set(suppressions.suppressions.map(\.key))
+    return findings.filter { !keys.contains($0.suppressionKey) }
 }
